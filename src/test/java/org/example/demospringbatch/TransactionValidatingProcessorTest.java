a81f8cf95e45fb571008301dc696092275c1205d
minhan <voongocminhan20072002@gmail.com>
2024-03-18 14:23:27 +0700

    add unit test for TransactionValidatingProcessor

---
+++ src/test/java/org/example/demospringbatch/TransactionValidatingProcessorTest.java
@@ -0,0 +1,34 @@
+package org.example.demospringbatch;
+
+import org.example.demospringbatch.batch.process.TransactionValidatingProcessor;
+import org.example.demospringbatch.models.Customer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.item.validator.ValidationException;
+
+public class TransactionValidatingProcessorTest {
+
+    @Test
+    public void shouldPassValidationWhenTransactionsAreAboveLimit() {
+        TransactionValidatingProcessor processor = new TransactionValidatingProcessor(5);
+        Customer customer = new Customer();
+        customer.setTransactions(6);
+        Assertions.assertDoesNotThrow(() -> processor.process(customer));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTransactionsAreBelowLimit() {
+        TransactionValidatingProcessor processor = new TransactionValidatingProcessor(5);
+        Customer customer = new Customer();
+        customer.setTransactions(2);
+        Assertions.assertNull(processor.process(customer));
+    }
+
+    @Test
+    public void shouldPassValidationWhenTransactionsAreEqualToLimit() {
+        TransactionValidatingProcessor processor = new TransactionValidatingProcessor(5);
+        Customer customer = new Customer();
+        customer.setTransactions(5);
+        Assertions.assertDoesNotThrow(() -> processor.process(customer));
+    }
+}
